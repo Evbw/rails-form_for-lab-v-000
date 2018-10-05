56982a4aac6ac6fb057b7435f4d7424cf8d5333c
Everett <everett.bowline@gmail.com>
2018-10-05 12:42:14 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -23,6 +23,14 @@ class SchoolClasses < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(post_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
   end
 
 end
